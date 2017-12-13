8e04aa9c330f2bd676a04eb7f95c24584c26cacb
Naivebug <xiaotiekeng@163.com>
2017-12-13 09:37:25 +0800

    path modify

--- script/_startme.py
+++ script/_startme.py
@@ -5,8 +5,8 @@
 import init
 # **后面加上/号
 #要么使用//号要么使用/号,不能使用/号
-ReadPath = "F:/work/git/excel/excel/";# 读取你的Excel表的路径,
-PathOut = "F:/work/git/excel/excel/script/" #你导出的Excel表的路径,可直接导到你工程的目录
+ReadPath = "./../excel/";# 读取你的Excel表的路径,
+PathOut = "./../excel/script/" #你导出的Excel表的路径,可直接导到你工程的目录
 
 
 init.Start(ReadPath,PathOut)
